5e6755491c36b98c1f3eace383624d0d392cfd39
jkaramon <karamon@softcamp.cz>
2010-12-15 17:28:24 +0100

    Added Capybara patch to enable png screenshots

--- lib/common/patches.rb
+++ lib/common/patches.rb
@@ -4,4 +4,5 @@ require_relative 'patches/cancan'
 require_relative 'patches/mongo_mapper'
 require_relative 'patches/mail'
 require_relative 'patches/pop3'
+require_relative 'patches/capybara'
 
---
+++ lib/common/patches/capybara.rb
@@ -0,0 +1,17 @@
+class Capybara::Driver::Selenium
+  
+  # Saves png screenshot to the filename
+  def save_screenshot(filename)
+    base64_png = bridge.getScreenshot
+    png_string = Base64.decode64(base64_png)
+    File.open(filename, 'wb') {|f| f.write(png_string) }
+  end
+  
+  private 
+  # Returns native webdriver  bridge 
+  def bridge
+    browser.instance_variable_get('@bridge')
+  end
+
+end
+
