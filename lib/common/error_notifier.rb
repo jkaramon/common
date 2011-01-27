d8442ab3c92d5872dc3d70e4718646ebecfdf960
jkaramon <karamon@softcamp.cz>
2011-01-27 13:55:58 +0100

    Much better error notifications

--- lib/common/error_notifier.rb
+++ lib/common/error_notifier.rb
@@ -3,9 +3,10 @@ module ErrorNotifier
   # exc - error which should be send
   # additional_data - Hash to provide additional error info.
   def notify_error(exc, additional_data = {})
+    additional_data[:backtrace] ||= exception.backtrace.join("\n")
     HoptoadNotifier.notify(
       :error_class   => exc.class.to_s,
-      :error_message => "#{exc.message}\n#{exc.backtrace}",
+      :error_message => exc.message,
       :parameters    => additional_data
     )
   end 
