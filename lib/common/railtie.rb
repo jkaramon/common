93bda515ed39545a87c31910796ae60480cf9a71
Jaroslav Cizmar <jaroslav.cizmar@vanilladesk.com>
2011-02-02 16:54:59 +0100

    adding default_url_options[:protocol]

--- lib/common/railtie.rb
+++ lib/common/railtie.rb
@@ -46,7 +46,8 @@ module Common
       ActionMailer::Base.delivery_method = :test
       ActionMailer::Base.raise_delivery_errors = true
 
-      ActionMailer::Base.default_url_options = AppConfig.mailer[:default_url_options]
+      ActionMailer::Base.default_url_options[:host] = AppConfig.mailer[:default_url_options][:host]
+      ActionMailer::Base.default_url_options[:protocol] = AppConfig.mailer[:default_url_options][:protocol]
       ActionMailer::Base.perform_deliveries = true
 
       ActionMailer::Base.smtp_settings = SecureConfig.smtp_settings.symbolize_keys
