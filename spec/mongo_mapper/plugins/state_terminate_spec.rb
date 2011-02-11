8bbb57e3778e398e4d44741adf5bf4535118de82
Jaroslav Cizmar <jaroslav.cizmar@vanilladesk.com>
2011-02-11 09:20:12 +0100

    VDAPP-1593 Common - prepsat metodu paginate ve StateTerminate pluginu tak, aby odfiltrovala terminated zaznamy

--- spec/mongo_mapper/plugins/state_terminate_spec.rb
+++ spec/mongo_mapper/plugins/state_terminate_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 require 'state_machine'
 
-class TestModel
+class StateTerminateModel
   include MongoMapper::Document
   plugin MongoMapper::Plugins::StateTerminated
   
@@ -27,12 +27,41 @@ end
 describe "MongoMapper::Plugins::StateTerminated" do
   before(:each) do
     MongoMapper.database = 'rspec-common-test'
-    TestModel.collection.remove
-    @entity = TestModel.new
+    StateTerminateModel.collection.remove
+    @entity = StateTerminateModel.new
   end
 
   it "should be in terminated state after terminated" do
     @entity.do_terminate
     @entity.state.should == "terminated"
   end
+
+  it "method all should return 2 results from 3, one is terminated" do
+    terminated_entity = StateTerminateModel.new
+    terminated_entity.do_terminate
+    first_entity = StateTerminateModel.new
+    first_entity.do_create
+    second_entity = StateTerminateModel.new
+    second_entity.do_create
+    
+    StateTerminateModel.all.count.should == 2
+  end
+
+  it "paginate should return 1 result from 4 (two per page, one terminated, second page) " do
+    terminated_entity = StateTerminateModel.new
+    terminated_entity.do_terminate
+    first_entity = StateTerminateModel.new
+    first_entity.do_create
+    second_entity = StateTerminateModel.new
+    second_entity.do_create
+    third_entity = StateTerminateModel.new
+    third_entity.do_create
+    options = {
+      :per_page  => 2,
+      :page      => 2
+    }
+    StateTerminateModel.paginate(options).count.should == 1
+  end
+
+
 end
