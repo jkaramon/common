9dce263e49944bed49fc480d887d47f591d792d9
jkaramon <karamon@softcamp.cz>
2011-02-20 14:46:22 +0100

    Disable Hoptoad js notifier in dev and test envs

--- lib/common/railties/app/helpers/hoptoad_helper.rb
+++ lib/common/railties/app/helpers/hoptoad_helper.rb
@@ -1,6 +1,6 @@
 module HoptoadHelper
   def hoptoad_js_notifier
-    return "" if Rails.env.production?
+    return "" if Rails.env.production? || Rails.env.development? || Rails.env.devcached? || Rails.env.test?
     hoptoad_javascript_notifier
   end
   
