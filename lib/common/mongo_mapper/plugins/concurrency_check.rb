8e0ccce536d8c0ff7964d5d8c1981c95fafd628a
jkaramon <karamon@softcamp.cz>
2011-03-09 18:34:29 +0100

    Corrected concurrency check handling

--- lib/common/mongo_mapper/plugins/concurrency_check.rb
+++ lib/common/mongo_mapper/plugins/concurrency_check.rb
@@ -1,9 +1,15 @@
+class ConcurrencyError < StandardError
+
+end
+
 module MongoMapper
   module Plugins
     module ConcurrencyCheck
+      
+      
       def self.configure(model)
         model.class_eval do
-          key :_timestamp, String, :default => generate_timestamp
+          key :_timestamp, String
           before_save :_check_concurrency
         end
       end
@@ -15,14 +21,23 @@ module MongoMapper
           tnow = Time.now.utc
           "#{tnow.to_i}#{tnow.tv_usec}"
         end
-
+        
+        # Can be overriden by each class
+        # By default, disables concurrency check if running in job server mode
+        def disable_concurrency_check?
+          ENV['JOB_SERVER'] == "1"
+        end
+          
+        
       end
 
       module InstanceMethods
         def _check_concurrency
+          return if self.class.disable_concurrency_check?
           actual_version = self.class.find(self.id)
-          unless actual_version.nil?
-            raise "Document has been modified" unless self._timestamp == actual_version._timestamp
+          
+          unless actual_version.try(:_timestamp).nil?
+            raise ConcurrencyError unless self._timestamp == actual_version._timestamp
           end
           self._timestamp = self.class.generate_timestamp
         end
@@ -30,3 +45,4 @@ module MongoMapper
     end
   end
 end
+
