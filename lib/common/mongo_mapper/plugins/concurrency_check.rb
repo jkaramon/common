45d596ce8f6c84cc1705368dee91e1dd606b595d
jkaramon <karamon@softcamp.cz>
2011-03-10 09:51:49 +0100

    Disable concurrency check on new document

--- lib/common/mongo_mapper/plugins/concurrency_check.rb
+++ lib/common/mongo_mapper/plugins/concurrency_check.rb
@@ -3,48 +3,49 @@
 module MongoMapper
   module Plugins
     module ConcurrencyCheck
-      
-      
+
+
       def self.configure(model)
         model.class_eval do
           key :_timestamp, String
-          before_validation :_check_concurrency, :on => :update
+          validate :_check_concurrency, :on => :update
         end
       end
 
-     
+
 
       module ClassMethods
         def generate_timestamp
           tnow = Time.now.utc
           "#{tnow.to_i}#{tnow.tv_usec}"
         end
-        
+
         # Can be overriden by each class
-        # By default, disables concurrency check if running in job server mode
+        # Default is false (enabled)
         def disable_concurrency_check?
-          ENV['JOB_SERVER'] == "1"
+          false  
         end
-          
-        
+
+
       end
 
       module InstanceMethods
-        def trigger_concurrency_check_error
-          self.errors.add(:id, "#{self.class.human_name} has been modified by someone else! Try reopen it and save again.")
-        end
 
         def _check_concurrency
-          return if self.class.disable_concurrency_check?
+          return if self.new? || self.class.disable_concurrency_check?
           actual_version = self.class.find(self.id)
-          
-          unless actual_version.try(:_timestamp).nil?
-            unless self._timestamp == actual_version._timestamp
-              self.trigger_concurrency_check_error
-            end
+
+          if actual_version.try(:_timestamp).present? and self._timestamp != actual_version._timestamp
+            self.trigger_concurrency_check_error
           end
           self._timestamp = self.class.generate_timestamp
         end
+
+
+        def trigger_concurrency_check_error
+          self.errors.add(:base, "#{self.class.human_name} has been modified by someone else! Try reopen it and save again.")
+        end
+
       end
     end
   end
--- spec/mongo_mapper/plugins/concurrency_check_spec.rb
+++ spec/mongo_mapper/plugins/concurrency_check_spec.rb
@@ -14,6 +14,20 @@ describe "MongoMapper::Plugins::ConcurrencyCheck" do
     @entity.save!
   end
 
+  it "unsaved entity should skip concurrency check" do
+    unsaved = ConcurrencyCheckModel.new(:name => "Name1")
+    unsaved.should be_valid
+    unsaved.save.should be_true
+  end
+
+  it "unsaved entity should skip concurrency check even if update_attributes called" do
+    unsaved = ConcurrencyCheckModel.new(:name => "Name1")
+    unsaved.update_attributes(:name => 'Name2')
+    unsaved.should be_valid
+    unsaved.save.should be_true
+  end
+
+
   it "should check concurrency update" do
     first = ConcurrencyCheckModel.find(@entity.id)
     second = ConcurrencyCheckModel.find(@entity.id)
@@ -21,5 +35,6 @@ describe "MongoMapper::Plugins::ConcurrencyCheck" do
     second.name = "2 subject"
     first.save!
     second.should_not be_valid
+    second.errors.full_messages.first.should include("has been modified by someone else!") 
   end
 end
