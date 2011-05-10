30dbd346bea01f7e851dcd650145a9a2976d84c2
jkaramon <karamon@softcamp.cz>
2011-05-10 11:40:49 +0200

    Added Regexp hex color pattern

--- lib/common/regex.rb
+++ lib/common/regex.rb
@@ -9,5 +9,8 @@ module Regex
 
   # Alphanumeric, starts with an alphabet and contains no special characters other than underscore or dash
   SUBDOMAIN = /^([a-zA-Z])[a-zA-Z_-]*[\w_-]*[\S]$|^([a-zA-Z])[0-9_-]*[\S]$|^[a-zA-Z]*[\S]$/i
+  
+  # Matches valid hexadecimal colors, 3 or 6 hexdigits only. Matches both lower and upper case.
+  HEX_COLOR = /^#([0-9a-f]{3}){1,2}$/
 
 end
