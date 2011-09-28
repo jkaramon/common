3d46e40584a523bec60105f2663525c72bc70263
Pavel Ondrousek <pavel.ondrousek@gmail.com>
2011-09-29 01:21:40 +0200

    VDAPP-2934 - integrace existujicich migraci

--- lib/common/db_seed/yaml_seed_store.rb
+++ lib/common/db_seed/yaml_seed_store.rb
@@ -9,7 +9,7 @@ module YAMLSeedStore
   # returns hash with data from yaml
   def load_from_store(seed_klass)
     raise "Class should implement :seed_template_name accessor" unless self.respond_to?(:seed_template_name)
-    name = seed_klass.to_s.underscore 
+    name = seed_klass.to_s.underscore.gsub("/","__") 
     fullname = store_path(name, seed_template_name)
     fullname = store_path(name) unless File.exist?( fullname )
     raise "Cannot find  '#{fullname}' file for data seed!"  unless File.exist?( fullname )
@@ -22,7 +22,7 @@ module YAMLSeedStore
   # returns hash with data from yaml
   def save_to_store(seed_klass, data)
     raise "Class should implement :seed_template_name accessor" unless self.respond_to?(:seed_template_name)
-    name = seed_klass.to_s.underscore 
+    name = seed_klass.to_s.underscore.gsub("/","__")
     fullname = store_path(name, seed_template_name)
     File.open(fullname, 'w') {|f| f.write(data) }
   end
